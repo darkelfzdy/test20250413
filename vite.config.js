cd812bbbd9e5242c532f91644bae1c7297622939
darkelfzdy <darkelfzdy@qq.com>
2025-04-16 10:30:02 +0800

    修改了设置允许域名

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    hmr: {
+      host: '5173.zdyhh.top'
+    },
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['*']
+    }
+  }
 })
